264db5e76dadef027ba37fa8e085cf8efc08e2fb
Tika Raj Bista <tika.raj.bista@gmail.com>
2018-04-04 07:03:18 -0600

    Lesson3_2 is done!

--- Lesson3_2/app/src/main/java/com/example/tikarajbista/lesson3_2/MainActivity.java
+++ Lesson3_2/app/src/main/java/com/example/tikarajbista/lesson3_2/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.tikarajbista.lesson3_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList arr = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    protected void decideFood(View v){
+        TextView food = (TextView) findViewById(R.id.textFood);
+        food.setText("raj");
+        //double r = Math.random() * 5 ;
     }
 }
