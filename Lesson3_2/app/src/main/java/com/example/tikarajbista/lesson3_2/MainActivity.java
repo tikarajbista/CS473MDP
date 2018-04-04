4a315387a25bca8a1d5c7e6ccb789908ba95b176
Tika Raj Bista <tikarajbista@gmail.com>
2018-04-04 12:25:13 -0500

    finilize Lessson3

--- Lesson3_2/app/src/main/java/com/example/tikarajbista/lesson3_2/MainActivity.java
+++ Lesson3_2/app/src/main/java/com/example/tikarajbista/lesson3_2/MainActivity.java
@@ -3,20 +3,34 @@ package com.example.tikarajbista.lesson3_2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList arr = new ArrayList<String>();
+   ArrayList foodList = new ArrayList<String>(Arrays.asList("Hamburger", "Pizza", "Mexican", "American", "Chinese"));
+   private TextView tv;
+   private EditText et;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textFood);
+        et = (EditText) findViewById(R.id.addFood);
     }
+
+    // Method to decide food ( randomly selects an item from foodList)
     protected void decideFood(View v){
-        TextView food = (TextView) findViewById(R.id.textFood);
-        food.setText("raj");
-        //double r = Math.random() * 5 ;
+        int r = (int)(Math.random() * foodList.size());
+        tv.setText(foodList.get(r).toString());
+    }
+
+    // Add new food on ArrayList
+    protected void addFood(View v){
+        String txt = et.getText().toString();
+        foodList.add(txt);
     }
 }
