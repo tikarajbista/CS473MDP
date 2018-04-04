6e53cd05f3eed625479146ce2fd21494b3843c39
Tika Raj Bista <tikarajbista@gmail.com>
2018-04-04 15:13:27 -0500

    Lesson4 is initialized

---
+++ W1L4/app/src/main/java/com/example/a986034/w1l4/User.java
@@ -0,0 +1,10 @@
+package com.example.a986034.w1l4;
+
+public class User {
+    private String userName;
+    private String password;
+    public User(String name, String pass){
+        this.userName = name;
+        this.password = pass;
+    }
+}
